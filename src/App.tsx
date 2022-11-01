fc190e27e3e2ea841eb4f8e0abe255b9c225c9d2
Serhii Trachuk <s.trachuk@apiko.com>
2022-11-01 19:26:59 +0200

    Add feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Banner } from "./common/components/banner/banner.component";
 import { Header } from "./common/components/header/header.component";
-import { Article } from "./modules/feed/components/article/article.component";
+import { Feed } from "./modules/feed/components/feed/feed.component";
 
 interface AppProps {}
 
@@ -9,8 +9,8 @@ export const App: FC<AppProps> = () => {
   return (
     <div>
       <Header />
-      <Banner /> 
-      <Article />
+      <Banner />
+      <Feed />
     </div>
   );
 };
